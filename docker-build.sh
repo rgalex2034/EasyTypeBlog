00a68b07aa0ff0e42f4ed08485fdb87c502635bd
Alex Ruiz <rgalex2034@hotmail.com>
2019-06-01 23:31:59 +0200

    Make docker-compose work with a default project name, and create container at the end.

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,5 @@
-docker-compose build
-docker run --rm --volume=$(pwd):/app blog_web npm install
-docker run --rm --volume=$(pwd):/app blog_web npm i @types/node
-docker run --rm --volume=$(pwd):/app blog_web npm run build
+docker-compose -p easyblog build
+docker run --rm --volume=$(pwd):/app easyblog_web npm install
+docker run --rm --volume=$(pwd):/app easyblog_web npm i @types/node
+docker run --rm --volume=$(pwd):/app easyblog_web npm run build
+docker-compose -p easyblog up --no-start
